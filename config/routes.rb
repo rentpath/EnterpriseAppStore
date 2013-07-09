27f954097beb77556c51ce3e5e785f4c563f5a39
mmuse <mmuse@primedia.com>
2013-07-09 09:24:51 -0400

    [JF/MM] app store

--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,8 @@
 RentPathApps::Application.routes.draw do
   devise_for :users
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  match '/plist', :to => redirect('public/plist/Apartments.plist')
 
-  # You can have the root of your site routed with "root"
- #root 'welcome#index'
-
-  # Example of regular route:
-  #get 'welcome/:id' => 'welcome#index'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
   resources :projects do
     resources :app_versions
   end
