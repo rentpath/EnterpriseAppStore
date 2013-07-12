247c8739196580a9302d496cb4f5fdc3efa3a4d7
mmuse <mmuse@primedia.com>
2013-07-12 11:16:17 -0400

    [] app store

--- app/models/app_version.rb
+++ app/models/app_version.rb
@@ -1,5 +1,5 @@
 class AppVersion < ActiveRecord::Base
-  attr_accessible :name, :version, :url_ipa, :url_plist, :url_icon, :notes, :app_plist, :app_ipa, :version_icon
+  attr_accessible :name, :version, :url_ipa, :url_plist, :url_icon, :notes, :app_plist, :app_plist_file_name, :app_ipa, :version_icon
 
   has_attached_file :app_plist,
                     :storage => :s3,
