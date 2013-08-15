32aa597d3c6abaad5fa9e3701593ff16360c499c
mmuse <mmuse@primedia.com>
2013-08-15 09:11:39 -0400

    [MM][000000] install mime-type for android

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -4,4 +4,4 @@
 # Mime::Type.register "text/richtext", :rtf
 Mime::Type.register_alias "text/html", :mobile
 
-Mime::Type.register "application/vnd.android.package-archive", :apk
+Mime::Type.register_alias "application/vnd.android.package-archive", :apk
